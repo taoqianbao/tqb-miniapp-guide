ee94fd8dbce136a841f5faaa31d1a6ca028338be
zhushengfeng1 <zhushengfeng1@wanda.cn>
2018-02-10 19:30:31 +0800

    微信小程序的Flex布局demo－4种必备常用的Flex布局模式

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ App({
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
     })
+
     // 获取用户信息
     wx.getSetting({
       success: res => {
@@ -33,7 +34,27 @@ App({
       }
     })
   },
+
   globalData: {
     userInfo: null
+  },
+
+  getUserInfo: function (cb) {
+    var that = this
+    if (this.globalData.userInfo) {
+      typeof cb == "function" && cb(this.globalData.userInfo)
+    } else {
+      //调用登录接口
+      wx.login({
+        success: function () {
+          wx.getUserInfo({
+            success: function (res) {
+              that.globalData.userInfo = res.userInfo
+              typeof cb == "function" && cb(that.globalData.userInfo)
+            }
+          })
+        }
+      })
+    }
   }
 })
